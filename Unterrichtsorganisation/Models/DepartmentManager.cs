f74dc05f2224314cef2d858c0e8ec69433d8e3b2
SaarYannic <yanniboy2003@gmail.com>
2026-09-22 15:48:48 +0200

    Models für die weiteren Tabellen hinzugefügt

---
+++ Unterrichtsorganisation/Models/DepartmentManager.cs
@@ -0,0 +1,6 @@
+﻿namespace Unterrichtsorganisation.Models {
+    public class DepartmentManager {
+        public string Department { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Unterrichtsorganisation/Models/Form.cs
@@ -0,0 +1,9 @@
+﻿namespace Unterrichtsorganisation.Models {
+    public class Form {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public int UserId { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public string ContentJson { get; set; }
+    }
+}
